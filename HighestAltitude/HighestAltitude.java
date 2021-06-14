01870948a0e08b2b6edee64f2cccf2d450854d61
Sarang Kurani <sarang.kurani@gmail.com>
2021-06-14 19:56:38 +0530

    Added description and boilerplate code for HighestAltitude.

---
+++ HighestAltitude/HighestAltitude.java
@@ -0,0 +1,14 @@
+package HighestAltitude;
+
+public class HighestAltitude {
+    
+    public static int largestAltitude(int [] gain) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int [] gain = {-5,1,5,0,-7};
+
+        System.out.println("Highest altitude of cyclist is: " + largestAltitude(gain));
+    }
+}
