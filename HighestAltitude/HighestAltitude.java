41550da777d0600a395ed90e7128e60dbed125d4
Sarang Kurani <sarang.kurani@gmail.com>
2021-06-14 20:32:38 +0530

    Added optimized solution for HighestAltitude.

--- HighestAltitude/HighestAltitude.java
+++ HighestAltitude/HighestAltitude.java
@@ -1,34 +1,21 @@
 package HighestAltitude;
 
-import java.util.Arrays;
-
 public class HighestAltitude {
     
     public static int largestAltitude(int [] gain) {
-        int altiudes [] = new int [gain.length + 1];
-        altiudes[0] = 0;
+       int max = 0;
+       int runningTotal = 0;
 
-        for (int i = 0; i < gain.length; i++) {
-            altiudes[i + 1] = gain[i] + altiudes[i];
-        }
+       for (int g : gain) {
+           runningTotal += g;
+           max = runningTotal > max ? runningTotal : max;
+       }
 
-        System.out.println(Arrays.toString(altiudes));
-
-        return findMax(altiudes);
-    }
-
-    public static int findMax (int [] arr) {
-        int max = arr[0];
-        
-        for (int i : arr) {
-            max = max > i ? max : i;
-        }
-
-        return max;
+       return max;
     }
 
     public static void main(String[] args) {
-        int [] gain = {-5,1,5,0,-7};
+        int [] gain = {-4,-3,-2,-1,4,3,2};
 
         System.out.println("Highest altitude of cyclist is: " + largestAltitude(gain));
     }
